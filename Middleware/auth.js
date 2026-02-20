9b634aa7b4d70d9e8aeb0bef721606e21b3c43c3
Zohaib Jaffar <xohaibshiekh@gmail.com>
2026-02-20 15:26:11 +0500

    Added the helmet,xss-clean and sanitize middleware for security

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -7,7 +7,6 @@ function authen(req,res,next){
     else if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
         token = req.headers.authorization.split(" ")[1]
     }
-    console.log(token)
     if(!token){
         return res.status(401).json({
             message : "Failed",
--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const routes = require("./Routes/routes.js")
 const DBconnection = require("./Config/DBconnection.js")
 const rateLimit = require("express-rate-limit")
+const xss = require("xss-clean")
+const mongoSanitize = require("express-mongo-sanitize")
+const helmet = require("helmet")
 const cookieParser = require("cookie-parser")
 
 const  CustomeError = require("./Utils/CustomeError.js")
@@ -13,7 +16,11 @@ const app = express()
 DBconnection(process.env.MONGO_URL)
 const PORT = process.env.PORT || 3000
 
+app.use(helmet())
 
+
+app.use(xss())
+app.use(mongoSanitize())
 const limiter = rateLimit({
     windowMs : 60 * 60 * 1000,
     max : 1000,
@@ -23,6 +30,8 @@ const limiter = rateLimit({
 app.use('/api', limiter)
 
 app.use(express.json({limit : "5mb"}))
+app.use(mongoSanitize())
+app.use(xss())
 app.use(cookieParser())
 
 app.use("/api/v1/",routes)
